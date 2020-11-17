27bd5d3d0900468d4ac8a38585c886b658c0c65b
PolinaShalak <polinakraft76@gmail.com>
2020-11-17 18:03:13 +0300

    homework p. 1-8

---
+++ main.js
@@ -0,0 +1,25 @@
+let user = 'John Doe';
+console.log(user);
+let student = 'Polina';
+console.log(student);
+user = student;
+console.log(user); // Polina
+
+let test = 1;
+test++;
+test +='1';
+console.log(test);  // 21
+test -= 1;
+console.log(test); // 2
+test = Boolean(test) ;
+console.log(test); // true
+console.log(typeof test);
+
+let array1 = [2, 3, 5, 8];
+let productOfNumbers = array1[0];
+for (let i = 1; i < array1.length; i++) {
+    productOfNumbers *= array1[i];
+}
+console.log(productOfNumbers);
+
+
