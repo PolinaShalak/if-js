ae9a9992101f1300b0a8d1a8e0d4160b3a6daf92
PolinaShalak <polinakraft76@gmail.com>
2020-11-18 15:36:10 +0300

    add arrays

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ test = Boolean(test);
 console.log(test); // true
 console.log(typeof test);
 
+// произведение элементов
 let array1 = [2, 3, 5, 8];
 let productOfNumbers = array1[0];
 for (let i = 1; i < array1.length; i++) {
@@ -22,3 +23,17 @@ for (let i = 1; i < array1.length; i++) {
 }
 console.log(productOfNumbers);
 
+//  элементы массива, которые больше 5, но меньше 10
+let array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < array2.length; i++) {
+    if (array2[i] > 5 && array2[i] < 10 ){
+        console.log(array2[i]);
+    }
+}
+
+// четные элементы массива
+for (let i = 0; i < array2.length; i++) {
+    if (array2[i] % 2 === 0 ){
+        console.log(array2[i]);
+    }
+}
