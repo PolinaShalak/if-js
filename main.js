8241dc67096f381eca4f04d06512024ddc0a2744
PolinaShalak <polinakraft76@gmail.com>
2020-11-19 19:59:14 +0300

    fixed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let user = 'John Doe';
 console.log(user);
-let student = 'Polina';
+const student = 'Polina';
 console.log(student);
 user = student;
 console.log(user); // Polina
@@ -8,32 +8,32 @@ console.log(user); // Polina
 let test = 1;
 test++;
 test += '1';
-console.log(test);  // 21
+console.log(test); // 21
 test -= 1;
-console.log(test); // 2
+console.log(test); // 20
 test = Boolean(test);
 console.log(test); // true
 console.log(typeof test);
 
 // произведение элементов
-let array1 = [2, 3, 5, 8];
+const array1 = [2, 3, 5, 8];
 let productOfNumbers = array1[0];
 for (let i = 1; i < array1.length; i++) {
-    productOfNumbers *= array1[i];
+  productOfNumbers *= array1[i];
 }
 console.log(productOfNumbers);
 
 //  элементы массива, которые больше 5, но меньше 10
-let array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+const array2 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < array2.length; i++) {
-    if (array2[i] > 5 && array2[i] < 10 ){
-        console.log(array2[i]);
-    }
+  if (array2[i] > 5 && array2[i] < 10) {
+    console.log(array2[i]);
+  }
 }
 
 // четные элементы массива
 for (let i = 0; i < array2.length; i++) {
-    if (array2[i] % 2 === 0 ){
-        console.log(array2[i]);
-    }
+  if (array2[i] % 2 === 0) {
+    console.log(array2[i]);
+  }
 }
