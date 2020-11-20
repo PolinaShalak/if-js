121301c184f41bf23de116e5f23d9d4dcd7e570c
PolinaShalak <71837845+PolinaShalak@users.noreply.github.com>
2020-11-20 10:58:03 +0300

    Lesson 1 (#2)
    * created
    
    * created
    
    * Create README.md
    
    * fixed
    
    * homework p. 1-8
    
    * homework p. 1-8
    
    * homework p. 1-8
    
    * add arrays
    
    * fixed

---
+++ main.js
@@ -0,0 +1,39 @@
+let user = 'John Doe';
+console.log(user);
+const student = 'Polina';
+console.log(student);
+user = student;
+console.log(user); // Polina
+
+let test = 1;
+test++;
+test += '1';
+console.log(test); // 21
+test -= 1;
+console.log(test); // 20
+test = Boolean(test);
+console.log(test); // true
+console.log(typeof test);
+
+// произведение элементов
+const array1 = [2, 3, 5, 8];
+let productOfNumbers = array1[0];
+for (let i = 1; i < array1.length; i++) {
+  productOfNumbers *= array1[i];
+}
+console.log(productOfNumbers);
+
+//  элементы массива, которые больше 5, но меньше 10
+const array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < array2.length; i++) {
+  if (array2[i] > 5 && array2[i] < 10) {
+    console.log(array2[i]);
+  }
+}
+
+// четные элементы массива
+for (let i = 0; i < array2.length; i++) {
+  if (array2[i] % 2 === 0) {
+    console.log(array2[i]);
+  }
+}
