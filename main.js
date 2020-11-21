39d29cfe2e26d05061a71fde9f10320a8a148e8d
PolinaShalak <polinakraft76@gmail.com>
2020-11-21 17:47:27 +0300

    add homework 3

--- main.js
+++ main.js
@@ -37,3 +37,59 @@ for (let i = 0; i < array2.length; i++) {
     console.log(array2[i]);
   }
 }
+
+// функция palindrome (cлово палиндром может читаться справа-налево и слева-направо одинаково)
+function checkPalindrome(word) {
+  const wordReverse = word.split('').reverse().join(''); // split() - разбивает строки на массив, reverse() - обратный порядок элементов, join('') - объедияет  элементы массива в строку
+  if (wordReverse === word) {
+    return true;
+  }
+  return false;
+}
+console.log(checkPalindrome('abcecba')); // true
+
+// функция, которая возвращает меньшее из чисел
+function min(a, b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+console.log(min(4, 7)); // 4
+
+// функция, которая возвращает большее из чисел
+function max(a, y) {
+  if (a > y) {
+    return a;
+  }
+  return y;
+}
+console.log(max(4, 7)); // 7
+
+// функция min с тернарным оператором
+function min1(a, b) {
+  return a < b ? a : b;
+}
+console.log(min1(10, 11));
+
+// функция max с тернарным оператором
+function max1(a, y) {
+  return a > y ? a : y;
+}
+console.log(max1(22, 5));
+
+// функция, которая заменяет все 0 на строку 'zero'
+const array3 = [];
+for (let i = 0; i < 10; i++) {
+  array3[i] = Math.floor(Math.random() * 101);
+}
+console.log(array3);
+
+for (let i = 0; i < array3.length; i++) {
+  let b = String(array3[i]);
+  if (b.endsWith('0')) {
+    b = b.replaceAll('0', 'zero');
+    array3[i] = b;
+  }
+}
+console.log(array3);
