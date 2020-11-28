b0032a07f8e9877ef98f18dbc54797852e66227a
PolinaShalak <polinakraft76@gmail.com>
2020-11-28 20:00:37 +0300

    .

--- main.js
+++ main.js
@@ -111,19 +111,19 @@ function sum3(a) {
 console.log(sum3(4)(10)); // 14
 
 // // абзацы меняют цвета из массива по клику
-// const text1El = document.getElementById('text1');
-// const text2El = document.getElementById('text2');
-// const text3El = document.getElementById('text3');
-// const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-//
-// const count = () => {
-//   let callNumber = 0;
-//   return (event) => {
-//     event.target.style.color = colors[callNumber];
-//     callNumber = callNumber > 3 ? 0 : callNumber + 1;
-//   };
-// };
-//
-// text1El.addEventListener('click', count());
-// text2El.addEventListener('click', count());
-// text3El.addEventListener('click', count());
+const text1El = document.getElementById('text1');
+const text2El = document.getElementById('text2');
+const text3El = document.getElementById('text3');
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+const count = () => {
+  let callNumber = 0;
+  return (event) => {
+    event.target.style.color = colors[callNumber];
+    callNumber = callNumber > 3 ? 0 : callNumber + 1;
+  };
+};
+
+text1El.addEventListener('click', count());
+text2El.addEventListener('click', count());
+text3El.addEventListener('click', count());
